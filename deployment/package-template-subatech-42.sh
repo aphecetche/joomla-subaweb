2029688f9b4572ded3bcd806d63bb4c8ac27f81e
laphecet <laurent.aphecetche@cern.ch>
2017-10-10 14:53:21 +0200

    mods for switch of local dev from mamp to docker + change of mathjax CDN address

--- deployment/package-template-subatech-42.sh
+++ deployment/package-template-subatech-42.sh
@@ -4,12 +4,15 @@ if [ ! -f template-subatech-42.version ]; then
   exit -1
 fi
 
-version=`cat template-subatech-42.version`
+currentversion=$(cat template-subatech-42.version)
+version=$(($currentversion+1))
+
+echo "currentversion=$currentversion version=$version"
 
 a=`pwd`
 
 dest="$HOME/Sites/joomla-dev/packages/subatech-42-$version.tar.gz"
-src="$HOME/Sites/joomla-dev/importlabo/mamp.subatech.in2p3.fr/templates/subatech-42"
+src="$HOME/Sites/joomla-dev/importlabo/www/templates/subatech-42-$currentversion"
 tmpdir="$HOME/Sites/joomla-dev/tmp/"
 
 rm -rf $tmpdir
@@ -20,7 +23,7 @@ cp -a $src $tmpdir
 
 cd $tmpdir
 
-mv subatech-42 subatech-42-$version
+mv subatech-42-$currentversion subatech-42-$version
 
 rm -f $dest
 
@@ -28,16 +31,16 @@ cd "$tmpdir/subatech-42-$version"
 
 d=`date "+%Y-%m-%d"`
 
-sed -i bck "s/MYNAME/subatech-42-$version/g" templateDetails.xml
-sed -i bck "s/MYVERSION/$version.0.0/g" templateDetails.xml
-sed -i bck "s/MYDATE/$d/g" templateDetails.xml
+sed -i bck "s/subatech-42-$currentversion/subatech-42-$version/g" templateDetails.xml
+sed -i bck "s/$currentversion.0.0/$version.0.0/g" templateDetails.xml
+# sed -i bck "s/MYDATE/$d/g" templateDetails.xml
 
-sed -i bck "s/subatech-42/subatech-42-$version/g" *.php
+sed -i bck "s/subatech-42-$currentversion/subatech-42-$version/g" *.php
 
-mv language/fr-FR/fr-FR.tpl_subatech-42.ini language/fr-FR/fr-FR.tpl_subatech-42-$version.ini 
-mv language/fr-FR/fr-FR.tpl_subatech-42.sys.ini language/fr-FR/fr-FR.tpl_subatech-42-$version.sys.ini
-mv language/en-GB/en-GB.tpl_subatech-42.ini language/en-GB/en-GB.tpl_subatech-42-$version.ini
-mv language/en-GB/en-GB.tpl_subatech-42.sys.ini language/en-GB/en-GB.tpl_subatech-42-$version.sys.ini
+mv language/fr-FR/fr-FR.tpl_subatech-42-$currentversion.ini language/fr-FR/fr-FR.tpl_subatech-42-$version.ini 
+mv language/fr-FR/fr-FR.tpl_subatech-42-$currentversion.sys.ini language/fr-FR/fr-FR.tpl_subatech-42-$version.sys.ini
+mv language/en-GB/en-GB.tpl_subatech-42-$currentversion.ini language/en-GB/en-GB.tpl_subatech-42-$version.ini
+mv language/en-GB/en-GB.tpl_subatech-42-$currentversion.sys.ini language/en-GB/en-GB.tpl_subatech-42-$version.sys.ini
 
 tar -c --exclude='._*' --exclude='.svn' --exclude='.DS_Store' --exclude='*.bak' --exclude='*~' -vzf $dest component.php changes.txt error.php favicon.ico index.html index.php template* js html images fonts css less language
 
--- templates/subatech-42/component.php
+++ templates/subatech-42/component.php
@@ -6,10 +6,9 @@ defined('_JEXEC') or die;
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 <head>
 <jdoc:include type="head" />
-<script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" type="text/javascript"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML" type="text/javascript"></script>
 
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/subatech-42/css/print.css" type="text/css" media="print,screen,projection" />
-<!--<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>-->
 </head>
 <body class="contentpane">
 	<div id="all">
