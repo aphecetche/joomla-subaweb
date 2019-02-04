6ecbb288c2a27a83aee335ce56e5e9e388d6b108
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-02-04 21:33:27 +0100

    Add fields to seminars database

---
+++ administrator/components/com_subatech/sql/updates/mysql/1.5.sql
@@ -0,0 +1,12 @@
+
+-- ALTER TABLE `#__seminars` ADD `author2` varchar(255) NOT NULL;
+-- ALTER TABLE `#__seminars` ADD `author_url2` TEXT NOT NULL;
+-- ALTER TABLE `#__seminars` ADD `date2` datetime NOT NULL;
+-- ALTER TABLE `#__seminars` ADD `summary2` longtext NOT NULL;
+-- ALTER TABLE `#__seminars` ADD `title2` text NOT NULL;
+-- ALTER TABLE `#__seminars` ADD `author_filiation2` text NOT NULL;
+-- ALTER TABLE `#__seminars` ADD `author_filiation_url2` text NOT NULL;
+
+
+  
+ 
